13a34b81e67decb950900ce3c5883a9b8f605edd
Oliver Tzeng <olivertzeng@proton.me>
2025-05-21 15:12:02 +0800

    wip

--- build_locale.sh
+++ build_locale.sh
@@ -1,5 +1,5 @@
-fd -t f -e py src | xargs rg -l "_\(|N\(_" >po/POTFILES
-fd -t f '.*\.xml$|.*\.in$' data >>po/POTFILES
+find src -type f -name "*.py" -exec grep -lE '_\(|N\(_' {} \; >po/POTFILES
+find data -type f \( -name "*.xml" -o -name "*.in" \) >>po/POTFILES
 xgettext -o po/nyarchassistant.pot $(cat po/POTFILES)
 cd po
 for file in $(fd -e po); do
